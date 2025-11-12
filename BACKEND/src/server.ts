2354dc1d227dfca00997d7adfd2e777aac149922
Vinícius Araújo <d2022003334@unifei.edu.br>
2025-11-12 16:52:06 -0300

    Contemplando as novas rotas no server

--- BACKEND/src/server.ts
+++ BACKEND/src/server.ts
@@ -3,21 +3,27 @@ import cors from "cors";
 import dotenv from "dotenv";
 import admin from "./config/firebase";
 import authRoutes from "./routes/authRoutes";
+import alunoRoutes from "./routes/alunoRoutes"; 
 
 dotenv.config();
 
 const app = express();
 
+//  Middlewares
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5173", // frontend local
     credentials: true,
   })
 );
 
 app.use(express.json());
-app.use("/auth", authRoutes);
 
+//  Rotas
+app.use("/auth", authRoutes);
+app.use("/alunos", alunoRoutes); 
+
+//  Rota de teste para verificar conexão com Firebase
 app.get("/", async (_, res) => {
   try {
     const projectId = admin.app().options.projectId;
@@ -25,7 +31,7 @@ app.get("/", async (_, res) => {
       message: "🔥 SGPA Backend rodando com Firebase Admin!",
       firebaseProject: projectId,
     });
-  } catch {
+  } catch (error) {
     res.status(500).json({ error: "Erro ao conectar com Firebase" });
   }
 });
