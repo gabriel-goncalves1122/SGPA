d559283961fea06711520c2a8ead99e631887f88
LuizFCSilva <luiz.silva@sharkdev.com.br>
2025-11-26 20:25:37 -0300

    feat: adicionando novas rotas para professores, projetos, tarefas, entregas e relatórios

--- FRONTEND/src/App.tsx
+++ FRONTEND/src/App.tsx
@@ -3,10 +3,15 @@ import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Alunos from "./pages/Alunos";
 import AlunosGrid from "./pages/AlunosGrid";
+import Professores from "./pages/Professores";
+import Projetos from "./pages/Projetos";
+import Tarefas from "./pages/Tarefas";
+import Entregas from "./pages/Entregas";
+import Relatorios from "./pages/Relatorios";
 import Layout from "./components/Layout";
 import { useAuth, AuthProvider } from "./contexts/AuthContext";
 
-const PrivateRoute = ({ children }: { children: JSX.Element }) => {
+const PrivateRoute = ({ children }: { children: React.ReactElement }) => {
   const { user, loading } = useAuth();
   if (loading) return <p>Carregando...</p>;
   return user ? <Layout>{children}</Layout> : <Navigate to="/" />;
@@ -39,6 +44,46 @@ const App = () => {
             element={
               <PrivateRoute>
                 <AlunosGrid />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/professores"
+            element={
+              <PrivateRoute>
+                <Professores />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/projetos"
+            element={
+              <PrivateRoute>
+                <Projetos />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/tarefas"
+            element={
+              <PrivateRoute>
+                <Tarefas />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/entregas"
+            element={
+              <PrivateRoute>
+                <Entregas />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/relatorios"
+            element={
+              <PrivateRoute>
+                <Relatorios />
               </PrivateRoute>
             }
           />
